dfa226909ad1a1c319b61378fdff8f914850cf09
Colin Brayton <colin.brayton17@gmail.com>
2021-01-23 19:21:39 -0300

    Initialize dependabot/npm_and_yarn/lodash-4.17.19

---
+++ src/app/al-mustansir/al-mustansir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-al-mustansir',
+  templateUrl: './al-mustansir.component.html',
+  styleUrls: ['./al-mustansir.component.scss']
+})
+export class AlMustansirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/night-visitors/night-visitors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-night-visitors',
+  templateUrl: './night-visitors.component.html',
+  styleUrls: ['./night-visitors.component.scss']
+})
+export class NightVisitorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

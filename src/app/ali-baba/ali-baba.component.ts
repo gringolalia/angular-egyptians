2f02ce9a3793c3faee9ce9a6e7ad54ae09163e70
Colin Brayton <colin.brayton17@gmail.com>
2021-01-23 19:21:27 -0300

    Initialize dependabot/npm_and_yarn/acorn-6.4.1

---
+++ src/app/ali-baba/ali-baba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ali-baba',
+  templateUrl: './ali-baba.component.html',
+  styleUrls: ['./ali-baba.component.scss']
+})
+export class AliBabaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/harun/harun.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-harun',
+  templateUrl: './harun.component.html',
+  styleUrls: ['./harun.component.scss']
+})
+export class HarunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
